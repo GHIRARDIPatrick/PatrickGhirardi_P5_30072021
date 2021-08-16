a353b953127a8c7c33adae5d71c485678cc280b3
GHIRARDI Patrick <82661732+GHIRARDIPatrick@users.noreply.github.com>
2021-08-16 20:03:07 +0200

    JS PANIER - Questionnaire  ok

--- src/index.js
+++ src/index.js
@@ -31,15 +31,71 @@ document.getElementById("produit-commande").addEventListener("click", function()
 
 // ________________________________________________________________________________________________________________
 // PANIER - Controle Formulaire
-document.getElementById("Cpostal").addEventListener("input", function() {
-    var code = document.getElementById("Cpostal");
-    var error = false;
-    if (!code.match(/^([0-9]){5}$/))
-        alert('Le code semble correct...');
-    else
-        alert('Mauvais format de code ! (4 chiffres)');
+document.getElementById("panier-prenom").addEventListener("input", function() {
+    var code = document.getElementById("panier-prenom").value;
+    if (!code.match(/^([a-zA-Z]+)$/)) {
+        document.getElementById("panier-prenom").style.backgroundColor = 'red';
+        document.getElementById("panier-validation").disabled = true;
+    } else {
+        document.getElementById("panier-prenom").style.backgroundColor = 'green';
+        document.getElementById("panier-validation").disabled = false;
+    }
 });
 
+document.getElementById("panier-nom").addEventListener("input", function() {
+    var code = document.getElementById("panier-nom").value;
+    if (!code.match(/^([a-zA-Z]+)$/)) {
+        document.getElementById("panier-nom").style.backgroundColor = 'red';
+        document.getElementById("panier-validation").disabled = true;
+    } else {
+        document.getElementById("panier-nom").style.backgroundColor = 'green';
+        document.getElementById("panier-validation").disabled = false;
+    }
+});
+
+document.getElementById("panier-adresse").addEventListener("input", function() {
+    var code = document.getElementById("panier-adresse").value;
+    if (!code.length > 0) {
+        document.getElementById("panier-adresse").style.backgroundColor = 'red';
+        document.getElementById("panier-validation").disabled = true;
+    } else {
+        document.getElementById("panier-adresse").style.backgroundColor = 'green';
+        document.getElementById("panier-validation").disabled = false;
+    }
+});
+
+document.getElementById("panier-cpostal").addEventListener("input", function() {
+    var code = document.getElementById("panier-cpostal").value;
+    if (!code.match(/^([a-zA-Z]+)$/)) {
+        document.getElementById("panier-cpostal").style.backgroundColor = 'red';
+        document.getElementById("panier-validation").disabled = true;
+    } else {
+        document.getElementById("panier-cpostal").style.backgroundColor = 'green';
+        document.getElementById("panier-validation").disabled = false;
+    }
+});
+
+document.getElementById("panier-ville").addEventListener("input", function() {
+    var code = document.getElementById("panier-ville").value;
+    if (!code.match(/^([a-zA-Z]+)$/)) {
+        document.getElementById("panier-ville").style.backgroundColor = 'red';
+        document.getElementById("panier-validation").disabled = true;
+    } else {
+        document.getElementById("panier-ville").style.backgroundColor = 'green';
+        document.getElementById("panier-validation").disabled = false;
+    }
+});
+
+document.getElementById("panier-email").addEventListener("input", function() {
+    var code = document.getElementById("panier-email").value;
+    if (!code.length > 0) {
+        document.getElementById("panier-email").style.backgroundColor = 'red';
+        document.getElementById("panier-validation").disabled = true;
+    } else {
+        document.getElementById("panier-email").style.backgroundColor = 'green';
+        document.getElementById("panier-validation").disabled = false;
+    }
+});
 
 
 
